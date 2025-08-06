8c193a282bca618a3f191d1e0bff2fb491773d99
Ahmad Khalid Saleh <aksaleh43@gmail.com>
2025-08-06 03:21:00 -0700

    Second commit

--- src/main/java/E02.java
+++ src/main/java/E02.java
@@ -2,6 +2,6 @@ public class E02 {
     public static void main(String[] args) {
 
         System.out.println("God bless you all");
-
+        System.out.println("The more you realize, the more it hurts");
     }
 }
