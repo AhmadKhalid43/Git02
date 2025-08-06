6fe42679d0196d2b9260714fa7ed11570f31820b
Ahmad Khalid Saleh <aksaleh43@gmail.com>
2025-08-06 04:32:04 -0700

    Fourth commit

--- src/main/java/E02.java
+++ src/main/java/E02.java
@@ -3,5 +3,7 @@ public class E02 {
 
         System.out.println("God bless you all");
         System.out.println("The more you realize, the more it hurts");
+        System.out.println("No pain no gain");
+        System.out.println("May all your dreams come true");
     }
 }
