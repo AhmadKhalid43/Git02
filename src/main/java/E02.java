2bcef823e543f0beb102f7bc5fe67468723324db
Ahmad Khalid Saleh <aksaleh43@gmail.com>
2025-08-07 02:20:32 -0700

    P1 Commit

---
+++ src/main/java/E02.java
@@ -0,0 +1,12 @@
+public class E02 {
+    public static void main(String[] args) {
+
+        System.out.println("God bless you all");
+        System.out.println("The more you realize, the more it hurts");
+        System.out.println("No pain no gain");
+        System.out.println("May all your dreams come true");
+
+        System.out.println("Changes");
+        System.out.println("New version");
+    }
+}
